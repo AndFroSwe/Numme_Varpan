1a4b6b49f468d9f87af1c5e955874e2a253bae46
AndFroSwe <gummianka88@gmail.com>
2015-09-30 17:56:50 +0200

    Plotting hermite for different h works

--- kurva.m
+++ kurva.m
@@ -9,27 +9,27 @@ u = start_vec(45, 19);
 bana_big = RKode(u, h*2);
 bana_small = RKode(u, h);
 
-n = 200;
-x = [];
-y = [];
+x1 = [];
+y1 = [];
+x2 = [];
+y2 = [];
+
 for n = 1:length(bana_big.x) - 1 
-    [x1, y1] = herm(bana_big, n);
-    plot(x1, y1)
+    [x, y] = herm(bana_big, n);
+    x1 = [x1 x];
+    y1 = [y1 y];
     hold on
 end
+
+plot(x1,y1)
+hold on
 
 for n = 1:length(bana_small.x) - 1 
-    [x1, y1] = herm(bana_small, n);
-    plot(x1, y1)
+    [x, y] = herm(bana_small, n);
+    x2 = [x2 x];
+    y2 = [y2 y];
     hold on
 end
 
-
-
-% [x2, y2] = herm(bana_small, n);
-% 
-% plot(x1, y1);
-% hold on 
-% plot(x1, y2);
-% legend('Big', 'Small')
+plot(x2,y2)
 
