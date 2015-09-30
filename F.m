2f68848b68dfd9074de0a86356338f30a2f534a1
AndFroSwe <gummianka88@gmail.com>
2015-09-30 14:55:45 +0200

    Working program

--- F.m
+++ F.m
@@ -4,7 +4,9 @@ function F = F (t, u)
 global uw
 kx = 0.020;  % Air resistance x-dir
 ky = 0.065;  % Air resistance y-dir
-V = sqrt(u(2)^2 + u(4)^2);   % Total velocity
+xvel = u(2);
+yvel = u(4);
+V = sqrt((xvel-uw)^2 + yvel^2);   % Total velocity
 
-F = [u(2) -kx*(u(2)-uw)*V u(4) -9.81-ky*u(4)*V];
+F = [xvel -kx*(xvel-uw)*V yvel -9.81-ky*yvel*V];
 end
