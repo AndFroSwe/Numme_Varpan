283b7add294b5c04b20f8abd648554f71f09834b
AndFroSwe <gummianka88@gmail.com>
2015-10-01 13:15:01 +0200

    Added text in graph in kurva

--- kurva.m
+++ kurva.m
@@ -1,10 +1,10 @@
 % Print nice Hermite curve between 2 points
 clear all, close all, clc
 
+% Define parameters
 global uw
 uw = 0;
-
-h = 0.1;
+h = 0.005;
 u = start_vec(45, 19);
 bana_big = RKode(u, h*2);
 bana_small = RKode(u, h);
@@ -14,6 +14,7 @@ y1 = [];
 x2 = [];
 y2 = [];
 
+% Get interpolation for 2*h
 for n = 1:length(bana_big.x) - 1 
     [x, y] = herm(bana_big, n);
     x1 = [x1 x];
@@ -24,6 +25,7 @@ end
 plot(x1,y1)
 hold on
 
+% Get interpolation for h
 for n = 1:length(bana_small.x) - 1 
     [x, y] = herm(bana_small, n);
     x2 = [x2 x];
@@ -32,4 +34,10 @@ for n = 1:length(bana_small.x) - 1
 end
 
 plot(x2,y2)
-
+grid on
+leg_big = sprintf('h=%0.3f', 2*h);
+leg_small = sprintf('h=%0.3f', h);
+title('Bana stycvis interpolerad med Hermite-interpolering')
+legend({leg_big, leg_small})
+xlabel('x [m]')
+ylabel('y [m]')
