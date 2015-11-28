dba98b25ad237dfc4ac363a933408c9313e94e15
AndFroSwe <gummianka88@gmail.com>
2015-11-28 12:18:31 +0100

    Fixed hermite error estimation. Left to do: Add linear interpolation of time in throw and display as a table. Also, in metertable, something wierd is happening at longer step sizes with hermite, must check

--- herm.m
+++ herm.m
@@ -1,20 +1,20 @@
-function [xpos, v] = herm(bana, n)
+function [times, y_interp_all] = herm(bana, n, k)
+% Uses hermite to plot curve between interpolated points
 
-points = 100;
-xpos = [];
-v = [];
-x1 = bana.x(n);
-x2 = bana.x(n + 1);
+points = 100*k;
+times = [];
+y_interp_all = [];
+t1 = bana.time(n);
+t2 = bana.time(n + 1);
 
-
+% Loop over points, gradually increase t between points
 for i = 1:points
-    x = x1 + (x2-x1)*i/points;
-    p = Hermite(    x,...
-                    x1, x2,...
-                    bana.y(n), bana.y(n + 1),...
-                    bana.yvel(n), bana.yvel(n + 1));
-%    p = LinPol(bana.y(n), bana.y(n + 1), x1, x2, x);
-    v = [v p];
-    xpos = [xpos x];
-end
+    t = t1 + (t2-t1)*i/points;
+    y_interp = Hermite(     t,...
+                            t1, t2,...
+                            bana.y(n), bana.y(n + 1),...
+                            bana.yvel(n), bana.yvel(n + 1));      
 
+    y_interp_all = [y_interp_all y_interp];
+    times = [times t];
+end
